1214490fc3ceb7a71cb8b7f39dd7335be1dc8b9d
Eugene <eugenetham1994@gmail.com>
2022-08-03 14:57:41 +0800

    Chore/update form validation (#7)
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: remove delay option
    
    * chore: update

--- apps/dashboard/src/pages/main-page/util.tsx
+++ apps/dashboard/src/pages/main-page/util.tsx
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 import { faker } from '@faker-js/faker';
+import { CREDENTIAL } from '~/models/enums';
 
 interface Credential {
     userID: string;
@@ -7,15 +8,15 @@ interface Credential {
 }
 
 export const generateCredentials = (): Credential => {
-    let userID = localStorage.getItem("BROWSER_NOTIFY_UI_USERID");
-    let company = localStorage.getItem("BROWSER_NOTIFY_UI_COMPANY");
+    let userID = localStorage.getItem(CREDENTIAL.BROWSER_NOTIFY_UI_USERID);
+    let company = localStorage.getItem(CREDENTIAL.BROWSER_NOTIFY_UI_COMPANY);
     if (userID == null) {
         userID = `${nanoid()}_${faker.internet.email()}`;
-        localStorage.setItem("BROWSER_NOTIFY_UI_USERID", userID);
+        localStorage.setItem(CREDENTIAL.BROWSER_NOTIFY_UI_USERID, userID);
     }
     if (company == null) {
         company = faker.company.companyName();
-        localStorage.setItem("BROWSER_NOTIFY_UI_COMPANY", company)
+        localStorage.setItem(CREDENTIAL.BROWSER_NOTIFY_UI_COMPANY, company)
     }
 
     return {userID, company};
