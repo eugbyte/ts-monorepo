543cf87ec9957b1a4dff3076ce2b1b0e9e6a1fde
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-08-04 15:50:53 +0800

    chore: update

--- apps/dashboard/src/hooks/local-storage/local-storage.ts
+++ apps/dashboard/src/hooks/local-storage/local-storage.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+// note that storage event only trigger only works if other window changes the local storage
+// so, as a workaround, need to manually trigger the event with window.dispatchEvent(new Event("storage"));
 export const useLocalStorage = (itemName: string): [string, (item: string) => void] => {
     const [item, setItem] = useState<string>(localStorage.getItem(itemName) || "");
 
