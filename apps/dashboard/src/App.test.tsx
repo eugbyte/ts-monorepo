4745f23d601548b5a0101a9f548f72a5257ac0a8
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-08-05 18:10:56 +0800

    fix: nanoid

--- apps/dashboard/src/App.test.tsx
+++ apps/dashboard/src/App.test.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import * as cred from "~/libs/credentials/generate-credentials";
-
-jest.mock("nanoid");
 
 describe("test main page", () => {
-  beforeEach(() => {
-    // nanoid uses ESM, but jest does not support ESM yet
-    // https://github.com/ai/nanoid/issues/365#issuecomment-1154068295
-    jest.spyOn(cred, "generateCompany").mockReturnValue("Mock-Company");
-    jest.spyOn(cred, "generateUserID").mockReturnValue("123_john@mail.com");
-  });
-
-  it("test main page", () => {
+  it("test main page", async() => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = await screen.findByText(/Grant permission/i);
     expect(linkElement).toBeInTheDocument();
   })
 });
