6607f997495e8b0f101c9817ae30acc052b61cd7
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-08-06 14:51:07 +0800

    fix: service worker

--- libs/service-workers/src/notification/broadcast.ts
+++ libs/service-workers/src/notification/broadcast.ts
@@ -1,11 +1,11 @@
 import { BroadcastChannel as PolyFilledBroadcastChannel } from "broadcast-channel";
 
-let broadcast: BroadcastChannel;
+let broadcast: BroadcastChannel; 
 if ("BroadcastChannel" in window) {
+  console.log("browser supports BroadcastChannel");
   broadcast = new BroadcastChannel("BROSWER_NOTIFY_UI");
 } else {
   broadcast = new PolyFilledBroadcastChannel("BROSWER_NOTIFY_UI", {
-    type: "native",
     webWorkerSupport: true,
   }) as any;
 }
