ef89dc6e28a8de0329a6472db0ce6e0520163a10
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-08-09 00:25:25 +0800

    chore: update

--- apps/dashboard/src/pages/main-page/main-page.test.tsx
+++ apps/dashboard/src/pages/main-page/main-page.test.tsx
@@ -3,15 +3,6 @@ import { render, screen } from "@testing-library/react";
 import { MainPage } from "./main-page";
 
 describe("test main page", () => {
-  beforeEach(() => {
-    // global.Notification = {
-    //   requestPermission: function(cb?: NotificationPermissionCallback) {
-    //     return Promise.resolve("granted");
-    //   }
-    // } as any;
-    global.navigator;
-  });
-
   afterEach(() => {
     jest.resetAllMocks();
   });
@@ -20,5 +11,21 @@ describe("test main page", () => {
     console.log({ navigator: global.navigator.permissions });
     render(<MainPage />);
     expect(screen.findByText(/Grant permission/i)).resolves.toBeInTheDocument();
+  });
+
+  it("Instructions to allow permission should be displayed when permission is blocked", () => {
+    console.log({ navigator: global.navigator.permissions });
+    global.Notification = {
+      permission: "denied",
+    } as any;
+    render(<MainPage />);
+    expect(
+      screen.findByText(/You have blocked notifications from the website/i)
+    ).resolves.toBeInTheDocument();
+    expect(
+      screen.findByText(
+        /To re-enable, click on the relevant icon on the left of the address bar, and edit the settings./i
+      )
+    ).resolves.toBeInTheDocument();
   });
 });
