2a7b88302c47246e076963ffc47d1628ea76faf9
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-08-09 01:38:28 +0800

    chore: fix exports

--- apps/dashboard/src/pages/main-page/main-page.test.tsx
+++ apps/dashboard/src/pages/main-page/main-page.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { MainPage } from "./main-page";
+import * as notifyLib from "@browser-notify-ui/service-workers";
 
 describe("test main page", () => {
   afterEach(() => {
@@ -8,24 +9,35 @@ describe("test main page", () => {
   });
 
   it("'Grant permission' title should be displayed", () => {
-    console.log({ navigator: global.navigator.permissions });
     render(<MainPage />);
     expect(screen.findByText(/Grant permission/i)).resolves.toBeInTheDocument();
   });
 
-  it("Instructions to allow permission should be displayed when permission is blocked", () => {
-    console.log({ navigator: global.navigator.permissions });
+  it("section 2 should show when permission is granted", () => {
     global.Notification = {
-      permission: "denied",
+      permission: "granted",
     } as any;
     render(<MainPage />);
     expect(
       screen.findByText(/You have blocked notifications from the website/i)
     ).resolves.toBeInTheDocument();
+    expect(screen.findByText(/2. Subscribe/i)).resolves.toBeInTheDocument();
+  });
+
+  it("section 3 and section 4 should show when section 2 successfully subscribes", () => {
+    global.Notification = {
+      permission: "granted",
+    } as any;
+    jest.spyOn(notifyLib, "subscribe").mockResolvedValue({});
+    render(<MainPage />);
+
+    const addButton = screen.getByRole("button", {
+      name: /Subscribe/i,
+    });
+    fireEvent(addButton, new MouseEvent("click", { bubbles: true }));
     expect(
-      screen.findByText(
-        /To re-enable, click on the relevant icon on the left of the address bar, and edit the settings./i
-      )
+      screen.findByText(/3. Create notifications/i)
     ).resolves.toBeInTheDocument();
+    expect(screen.findByText(/4. Send!/i)).resolves.toBeInTheDocument();
   });
 });
