7ec6f854a8e833cd038b6b1d6b5fb7639103d841
Eugene <eugenetham1994@gmail.com>
2022-08-11 13:16:40 +0800

    Chore/refactor push 2 (#16)
    * test/components
    
    * chore: update

--- libs/service-workers/src/notification/push-message.ts
+++ libs/service-workers/src/notification/push-message.ts
@@ -14,28 +14,33 @@ export const pushMessage = async (
   apiKey: string,
   notifySecretName: string,
   notifySecretValue: string,
-  company: string,
-  userID: string,
-  title: string,
-  body: string,
-  icon?: string
+  info: MessageInfo
 ): Promise<Record<string, string>> => {
   const url = "http://localhost:7071/api/notifications";
 
-  const messageInfo: MessageInfo = {
-    userID,
-    company,
-    notification: {
-      title,
-      body,
-      icon,
-    },
-  };
+  if (
+    apiKey.trim() === "" ||
+    notifySecretName.trim() === "" ||
+    notifySecretValue.trim() === ""
+  ) {
+    throw new Error("keys, secrets cannot be empty");
+  }
+  if (info.userID.trim() === "" || info.company.trim() === "") {
+    throw new Error("fields cannot be empty");
+  }
+  if (
+    info.notification == null ||
+    info.notification.title.trim() === "" ||
+    info.notification.body.trim() === ""
+  ) {
+    throw new Error("fields cannot be empty");
+  }
+
   const headers: AxiosRequestHeaders = {
     "API-Key": apiKey,
     "Notify-Secret-Name": notifySecretName,
     "Notify-Secret-Value": notifySecretValue,
   };
-  const result = await axios.post(url, messageInfo, { headers });
+  const result = await axios.post(url, info, { headers });
   return result.data;
 };
