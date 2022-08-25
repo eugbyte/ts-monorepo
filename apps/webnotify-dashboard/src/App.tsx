39a55f535f54d2e04a5ed386289317927e4c7803
Eugene <eugenetham1994@gmail.com>
2022-08-25 11:56:59 +0800

    Chore/rename folder and tab title (#25)
    * chore: rename tab title
    
    * chore: rename
    
    * chore: update
    
    * chore: update

--- apps/webnotify-dashboard/src/App.tsx
+++ apps/webnotify-dashboard/src/App.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MainPage from "./pages/main-page";
 
 function App() {
+  useEffect(() => {
+    document.title = "Web Notification SaaS";
+  }, []);
   return (
     <div className="overflow-auto">
       <MainPage />
