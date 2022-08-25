95e0b5416101b6c5f9a38f1dedbd9de089fb465d
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-08-25 16:37:18 +0800

    chore: update

--- apps/webnotify-dashboard/src/components/sections/about/about.tsx
+++ apps/webnotify-dashboard/src/components/sections/about/about.tsx
@@ -4,12 +4,13 @@ export const About: React.FC = () => {
   return (
     <section className="mt-10 mb-8 mx-1">
       <h1 className="text-2xl text-white font-bold text-center">
-        Web push notification SaaS
+        Web notification SaaS
       </h1>
       <p className="text-white break-words max-w-3xl text-justify">
-        As a customer, I want to receive notification updates on my computer
-        (e.g. food order), so that I can continue to focus on what I have been
-        doing on my computer without having to constantly check my phone.
+        As a customer, I want to receive push notification updates on my
+        computer (e.g. food order), so that I can continue to focus on what I
+        have been doing on my computer without having to constantly check my
+        phone.
       </p>
       <p className="text-white break-words max-w-3xl text-justify mt-5">
         Overall flow and demo below:
