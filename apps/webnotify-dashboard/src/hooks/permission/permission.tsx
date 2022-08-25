6ee7c48f23b683244056002c9190469d62e45976
Eugene <eugenetham1994@gmail.com>
2022-08-25 16:06:37 +0800

    fix: ci (#26)
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * chore: update
    
    * chore: update
    
    * chore: remove clean up
    
    * chore: update

--- apps/webnotify-dashboard/src/hooks/permission/permission.tsx
+++ apps/webnotify-dashboard/src/hooks/permission/permission.tsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
  * React's virtual dom diffing only updates the actual dom based on changes in the virtual dom
  * The permission API affects the windows object, not the dom, and affects the actual tree, not the virtual tree.
  * Thus, useEffect(() => {}, [Notification.permission]) does not work
- * Workaround is to use setInterval and periodically poll for the permission status
+ * Workaround is to manually add event listener
  */
 export const usePermission = (): [
   NotificationPermission,
