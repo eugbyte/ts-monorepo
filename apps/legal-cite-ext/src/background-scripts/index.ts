429301e4d043b60601eff639cf4e761155023c85
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-10-15 13:16:51 +0800

    remove legal ext

--- apps/legal-cite-ext/src/background-scripts/index.ts
+++
@@ -1,7 +0,0 @@
-// IIFE
-(() => {
-  // Dependency injection
-  console.log("in background script");
-})();
-
-export {};
--- apps/legal-cite-ext/src/content-scripts/index.ts
+++
@@ -1,8 +0,0 @@
-// IIFE
-(() => {
-  // Dependency injection
-  console.log("in content script");
-  document.body.style.border = "5px solid red";
-})();
-
-export {};
