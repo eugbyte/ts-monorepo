7d7b98733e6626716b00c33de661c1d208af4f96
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-10-15 13:16:51 +0800

    remove legal ext

--- apps/legal-cite-ext/README.md
+++
@@ -1,7 +0,0 @@
-# About
-A simple React starter template for the monorepo for a web extension, with pre-configured:
-* TypeScript
-* webpack
-* tailwind
-* eslint
-* jest
--- apps/legal-cite-ext/src/components/banner/banner.test.tsx
+++
@@ -1,7 +0,0 @@
-import { Banner } from "./banner";
-import { render, screen } from "@testing-library/react";
-
-it("test banner", () => {
-  render(<Banner />);
-  expect(screen.getByText("Hello World")).toBeInTheDocument();
-});
