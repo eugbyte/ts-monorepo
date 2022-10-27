3f5961bedc63ef5a3d73df11a097e5fd74378324
DESKTOP-VVPEAA1\eugen <eugenetham1994@gmail.com>
2022-10-28 00:55:50 +0800

    chore: update

--- apps/legal-cite-ext/src/content-scripts/statutes/clipboard/write-html.ts
+++ apps/legal-cite-ext/src/content-scripts/statutes/clipboard/write-html.ts
@@ -1,6 +1,16 @@
+import copy from "copy-to-clipboard";
+
 export const writeHTML = async (content: string) => {
   const type = "text/html";
   const blob = new Blob([content], { type });
-  const item = new window.ClipboardItem({ [type]: blob });
-  await navigator.clipboard.write([item]);
+  // ClipboardItem is not supported in firefox
+  // execCommand is not supported in chrome
+  if ("ClipboardItem" in window) {
+    const item = new window.ClipboardItem({ [type]: blob });
+    await navigator.clipboard.write([item]);
+  } else {
+    copy(content, {
+      format: type,
+    });
+  }
 };
